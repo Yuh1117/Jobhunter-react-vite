fc68f1cd970df66ee74bfa42b756188c918980ec
Yuh <huypham3188@gmail.com>
2024-11-21 15:56:30 +0700

    new

---
+++ src/components/client/footer.client.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer style={{ padding: 15, textAlign: "center" }}>
+            {/* <div>Frontend React Typescript </div> */}
+        </footer>
+    )
+}
+
+export default Footer;
